9a4faf10076d9c7110e9db5a503c6496fb43147f
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-04-21 14:20:28 -0300

    Create veiculo.java

--- veiculo.java
+++ veiculo.java
@@ -1,14 +1,18 @@
 
-
 public class veiculo {
     private String modelo;
     private String marca;
     private boolean disponivel;
+    private int codigo;
+    public veiculo() {
+        
+    }
+    public int getCodigo() {
+        return codigo;
+    }
 
-    public veiculo(String modelo, String marca) {
-        this.modelo = modelo;
-        this.marca = marca;
-        this.disponivel = true;
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 
     public String getModelo() {
@@ -22,14 +26,16 @@ public class veiculo {
     public boolean isDisponivel() {
         return disponivel;
     }
-
+    
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
     public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
     }
     
 
 }
-
- 
-
-    
