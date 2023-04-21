ea9e1d0f6c2d5a0902af5f950196f9d6b7821e61
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-04-21 07:53:33 -0300

    Update repositoriocarro.java

--- repositoriocarro.java
+++ repositoriocarro.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 public class repositoriocarro {
     public List<veiculo> veiculos;
-    gerente gerente;
 
-   public  repositoriocarro() {
+    public  repositoriocarro() {
        this.veiculos = new ArrayList<veiculo>();
        veiculos.add(new veiculo("gol","volks"));
        veiculos.add(new veiculo("golf","volks"));
@@ -15,13 +14,18 @@ public class repositoriocarro {
        veiculos.add(new veiculo("gol quadrado","volks"));
        
 
-   }
-   public void cadastrarVeiculo(String modelo, String marca) {
+    }
+    public void cadastrarVeiculo(String modelo, String marca) {
     veiculo veiculo = new veiculo(modelo, marca);
     veiculos.add(veiculo);
     
 }
-   
+    public void listaveiculos(){
+        for (int i = 0; i <veiculos.size(); i++) {
+            System.out.println( "modelo:" + veiculos.get(i).getModelo() +"," + "marca:" + veiculos.get(i).getMarca());
+            
+          }
+    } 
 
 
 }
