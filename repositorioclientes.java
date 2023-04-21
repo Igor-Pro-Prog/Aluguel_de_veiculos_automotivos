2852fc66748326f3e53c7109d339f7520537a56f
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-04-21 14:15:14 -0300

    Update repositorioclientes.java

--- repositorioclientes.java
+++ repositorioclientes.java
@@ -1,26 +1,33 @@
 
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class repositorioclientes {
 
-     public List<cliente> clientes;
-
+    public List<cliente> clientes =  new ArrayList<cliente>();
+    cliente cliente = new cliente();
 
     public  repositorioclientes() {
-       this.clientes = new ArrayList<cliente>();
+       
 
         
 
     }
-    public void cadastrarCliente(String nome, String cpf) {
-        cliente cliente = new cliente(nome, cpf);
+    public void cadastrarCliente(cliente cliente) {
         clientes.add(cliente);
        
     }
+
+    public void deletarCliente(int numero){
+        for(cliente cliente  : clientes){
+            if(cliente.getNumero() == numero){
+                clientes.remove(cliente);
+            }
+        }
+    }
+
+
     public void listaclientes(){
         for (int i = 0; i <clientes.size(); i++) {
             System.out.println( "mome:" + clientes.get(i).getNome() +"," + "cpf:" + clientes.get(i).getCpf() );
