70301843994b493580c39888b950966be767797b
Kaio Brasil da Silva <112896928+kaioreidela@users.noreply.github.com>
2023-04-21 07:27:58 -0300

    Update repositorioclientes.java

--- repositorioclientes.java
+++ repositorioclientes.java
@@ -7,15 +7,19 @@ import java.util.List;
 public class repositorioclientes {
 
      public List<cliente> clientes;
-     gerente gerente;
+
 
     public  repositorioclientes() {
-        clientes = new ArrayList<>();
+       this.clientes = new ArrayList<cliente>();
 
         
 
     }
-    
+    public void cadastrarCliente(String nome, String cpf) {
+        cliente cliente = new cliente(nome, cpf);
+        clientes.add(cliente);
+       
+    }
 
 
 }
