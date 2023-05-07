69017aff77d27b17787bb623b21221592a4aca38
subrs100zero <112484364+subrs100zero@users.noreply.github.com>
2023-05-07 10:47:14 -0300

    Update repositorioclientes.java

--- repositorio/repositorioclientes.java
+++ repositorio/repositorioclientes.java
@@ -7,25 +7,33 @@ public class repositorioclientes {
     public List<cliente> clientes =  new ArrayList<cliente>();
     
     public repositorioclientes() {
-    	cliente c = new cliente("teste", 12345, "t", 321);
+    	cliente c = new cliente("fulano", 12345, "ful", 321);
     	clientes.add(c);
     }
     
     public void cadastrarCliente(cliente cliente) {
         clientes.add(cliente);   
+        System.out.println("\n\033[0;32mCliente cadastrado com sucesso!\033[0m");
     }
 
     public void deletarCliente(int cpf){
         for(cliente cliente  : clientes){       	
             if(cliente.getCpf() == cpf){
             	clientes.remove(cliente);
+            	System.out.println("\n\033[0;32mCliente Removido!\033[0m");
+            	break;
             }
         }
     }
     
     public void listar(){
-        for (int i = 0; i <clientes.size(); i++) {
-            clientes.get(i).mostrarDados();    
+    	System.out.println("CLIENTES CADASTRADOS:");
+    	if(clientes.size() > 0) {
+    		for (int i = 0; i <clientes.size(); i++) {
+                clientes.get(i).mostrarDados();
+    		}   
+        }else {
+        	System.out.println("NENHUM CLIENTE CADASTRADO NA LOJA");
         }
     }
     
